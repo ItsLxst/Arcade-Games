7d241f0f4f6a3e3fcb5d13d75b8e1e2445ab4f9b
Goknil Bilge <169390832+ItsLxst@users.noreply.github.com>
2025-12-11 15:56:32 +0300

    Fourth Commit: Highscore being saved

--- Snake_Game/scoreboard.py
+++ Snake_Game/scoreboard.py
@@ -3,12 +3,15 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Comic Sans MS", 24, "normal")
 
+with open("high_score.txt", "r") as file:
+    highest_score = int(file.read())
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        self.highscore = int(highest_score)
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -22,6 +25,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("high_score.txt", "w") as file:
+                file.write(str(self.highscore))
         self.score = 0
         self.update_scoreboard()
 
