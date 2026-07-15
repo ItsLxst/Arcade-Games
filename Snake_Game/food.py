e953e664a6ea7b4f0dfe3123987d86a4fe3816e5
Lost <169390832+ItsLxst@users.noreply.github.com>
2026-07-15 13:36:08 +0300

    Refactor food position boundaries in refresh method
    Updated food position boundaries to use FOOD_BOUNDARY constant.

--- Snake_Game/food.py
+++ Snake_Game/food.py
@@ -1,6 +1,8 @@
 from turtle import Turtle
 import random
 
+FOOD_BOUNDARY = 280
+
 class Food(Turtle):
 
     def __init__(self):
@@ -13,6 +15,6 @@ class Food(Turtle):
         self.refresh()
 
     def refresh(self):
-        random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
+        random_x = random.randint(-FOOD_BOUNDARY, FOOD_BOUNDARY)
+        random_y = random.randint(-FOOD_BOUNDARY, FOOD_BOUNDARY)
         self.goto(random_x, random_y)
