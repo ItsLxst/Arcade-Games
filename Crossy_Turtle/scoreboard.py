d0593a8d2ae9cbf022bf25471e47df6784216c05
Lost <169390832+ItsLxst@users.noreply.github.com>
2026-07-16 10:57:26 +0300

    Refactor scoreboard positions and update methods

--- Crossy_Turtle/scoreboard.py
+++ Crossy_Turtle/scoreboard.py
@@ -1,6 +1,10 @@
 from turtle import Turtle
 
 FONT = ("Comic Sans MS", 24, "normal")
+ALIGNMENT = "left"
+
+LEVEL_POSITION = (-280, 260)
+GAME_OVER_POSITION = (0, 0)
 
 
 class Scoreboard(Turtle):
@@ -10,17 +14,19 @@ class Scoreboard(Turtle):
         self.level = 1
         self.hideturtle()
         self.penup()
-        self.goto(-280,260)
+        self.goto(LEVEL_POSITION)
 
+        self.update_scoreboard()
 
-    def update(self):
+    def update_scoreboard(self):
         self.clear()
-        self.write(f"Level: {self.level}", align="left", font=FONT)
+        self.goto(LEVEL_POSITION)
+        self.write(f"Level: {self.level}", align=ALIGNMENT, font=FONT)
 
     def increase_level(self):
         self.level += 1
-        self.update()
+        self.update_scoreboard()
 
     def game_over(self):
-        self.goto(0,0)
+        self.goto(GAME_OVER_POSITION)
         self.write("GAME OVER!", align="center", font=FONT)
