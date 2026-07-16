fe8e608d3e3b79fd9690c9da2fa8662f06977149
Lost <169390832+ItsLxst@users.noreply.github.com>
2026-07-16 10:56:02 +0300

    Refactor CarManage class with constants for positioning

--- Crossy_Turtle/cars.py
+++ Crossy_Turtle/cars.py
@@ -4,7 +4,12 @@ import random
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-class CarManage():
+START_X = 300
+MIN_Y = -250
+MAX_Y = 250
+
+
+class CarManage:
 
     def __init__(self):
         self.cars = []
@@ -18,18 +23,21 @@ class CarManage():
 
     def create_car(self):
         rand_chance = random.randint(1, 6)
+
         if rand_chance == 1:
             car = Turtle(shape="square")
             car.shapesize(stretch_wid=1, stretch_len=2)
             car.penup()
             car.color(self.random_color())
-            random_y = random.randint(-250,250)
-            car.goto(300, random_y)
+
+            random_y = random.randint(MIN_Y, MAX_Y)
+            car.goto(START_X, random_y)
+
             self.cars.append(car)
 
     def move_cars(self):
         for car in self.cars:
             car.backward(self.car_speed)
 
-    def  level_up(self):
+    def level_up(self):
         self.car_speed += MOVE_INCREMENT
