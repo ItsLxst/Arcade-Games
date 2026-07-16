7c3702b8176709d8bb005dbf224c51724a2418f4
Lost <169390832+ItsLxst@users.noreply.github.com>
2026-07-16 10:52:51 +0300

    Refactor scoreboard update methods for clarity

--- Pong_Game/scoreboard.py
+++ Pong_Game/scoreboard.py
@@ -1,28 +1,38 @@
 from turtle import Turtle
 
+ALIGNMENT = "center"
+FONT = ("Comic Sans MS", 24, "normal")
+
+LEFT_SCORE_POSITION = (-100, 200)
+RIGHT_SCORE_POSITION = (100, 200)
+
+
 class Scoreboard(Turtle):
-    
+
     def __init__(self):
         super().__init__()
         self.color("white")
         self.penup()
         self.hideturtle()
+
         self.l_score = 0
         self.r_score = 0
-        self.update()
-           
-    def update(self):
+
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
         self.clear()
-        self.goto(-100, 200)
-        self.write(self.l_score, align="center", font=("Comic Sans MS", 24, "normal"))
-        self.goto(100, 200)
-        self.write(self.r_score, align="center", font=("Comic Sans MS", 24, "normal"))
-        
-        
+
+        self.goto(LEFT_SCORE_POSITION)
+        self.write(self.l_score, align=ALIGNMENT, font=FONT)
+
+        self.goto(RIGHT_SCORE_POSITION)
+        self.write(self.r_score, align=ALIGNMENT, font=FONT)
+
     def l_point(self):
         self.l_score += 1
-        self.update()
-        
+        self.update_scoreboard()
+
     def r_point(self):
         self.r_score += 1
-        self.update()
+        self.update_scoreboard()
