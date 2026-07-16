fc5379b5ca62d6adb71de42e90b17e9957a2a2d9
Lost <169390832+ItsLxst@users.noreply.github.com>
2026-07-16 10:52:16 +0300

    Refactor paddle movement distance to a constant

--- Pong_Game/paddle.py
+++ Pong_Game/paddle.py
@@ -1,19 +1,22 @@
 from turtle import Turtle
 
+MOVE_DISTANCE = 20
+
+
 class Paddle(Turtle):
 
-    def __init__(self,coordinat):
+    def __init__(self, position):
         super().__init__()
         self.shape("square")
         self.color("white")
         self.penup()
         self.shapesize(stretch_wid=5, stretch_len=1)
-        self.goto(coordinat)
+        self.goto(position)
 
     def go_up(self):
         y_cor = self.ycor()
-        self.goto(self.xcor(), y_cor + 20)
+        self.goto(self.xcor(), y_cor + MOVE_DISTANCE)
 
     def go_down(self):
         y_cor = self.ycor()
-        self.goto(self.xcor(), y_cor - 20)
+        self.goto(self.xcor(), y_cor - MOVE_DISTANCE)
