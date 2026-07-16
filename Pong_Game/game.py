a55f772c2023cda9869c7a1ca56b39de43a5b489
Lost <169390832+ItsLxst@users.noreply.github.com>
2026-07-16 10:50:56 +0300

    Refactor constants for screen and collision logic

--- Pong_Game/game.py
+++ Pong_Game/game.py
@@ -1,14 +1,22 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 from scoreboard import Scoreboard
 import time
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+WALL_LIMIT = 280
+PADDLE_COLLISION_DISTANCE = 50
+PADDLE_COLLISION_X = 320
+BALL_OUT_X = 380
+
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width=800, height=600)
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 screen.tracer(0)
 screen.title("Pong Game")
-
 
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
@@ -28,20 +36,26 @@ while game_on:
     ball.move()
 
     # Wall collision + bounce
-    if ball.ycor() > 280 or ball.ycor() < -280:
+    if ball.ycor() > WALL_LIMIT or ball.ycor() < -WALL_LIMIT:
         ball.y_bounce()
 
     # Paddle collision + bounce
-    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+    if (
+        ball.distance(right_paddle) < PADDLE_COLLISION_DISTANCE
+        and ball.xcor() > PADDLE_COLLISION_X
+    ) or (
+        ball.distance(left_paddle) < PADDLE_COLLISION_DISTANCE
+        and ball.xcor() < -PADDLE_COLLISION_X
+    ):
         ball.x_bounce()
 
-    # Right paddle miss + Game reset
-    if ball.xcor() > 380:
+    # Right paddle miss + score update
+    if ball.xcor() > BALL_OUT_X:
         ball.reset_pos()
         scoreboard.l_point()
 
-    # Left paddle miss + Game reset
-    if ball.xcor() < -380:
+    # Left paddle miss + score update
+    if ball.xcor() < -BALL_OUT_X:
         ball.reset_pos()
         scoreboard.r_point()
 
